5feb3abb51766f3580309a68f5e25343d764be0d
Serhii Marchenko <serhii.marchenko@Kaltura.com>
2020-02-24 03:09:25 +0200

    chore(REACH2-782): use ObjectUtils from Contrib

--- src/utils.ts
+++ src/utils.ts
@@ -173,24 +173,4 @@ export class CaptionAssetServeAction extends KalturaRequest<{url: string}> {
     }
   }
 
-  export function deepGet(obj: any, props: Array<string | number>, defaultValue?: any): any {
-    // If we have reached an undefined/null property
-    // then stop executing and return the default value.
-    // If no default was provided it will be undefined.
-    if (obj === undefined || obj === null) {
-        return defaultValue;
-    }
-
-    // If the path array has no more elements, we've reached
-    // the intended property and return its value
-    if (props.length === 0) {
-        return obj;
-    }
-
-    // Prepare our found property and path array for recursion
-    var foundSoFar = obj[props[0]];
-    var remainingProps = props.slice(1);
-
-    return deepGet(foundSoFar, remainingProps, defaultValue);
-}
 
