09eff569132603074381269526ed384446b1406c
yuval.keidar <keidaryu@gmail.com>
2021-06-30 11:01:53 +0300

    rollback to previous implementation

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -31,23 +31,6 @@ if [ "${TRAVIS_MODE}" = "release" ] || [ "${TRAVIS_MODE}" = "releaseCanary" ]; t
 elif [ "${TRAVIS_MODE}" = "deploy" ]; then
   echo "Deploy..."
 else
-    echo "Run standard-version"
-    npm run bump-canary
-    sha=$(git rev-parse --verify --short HEAD)
-    echo "Current sha ${sha}"
-    commitNumberAfterTag=$(git rev-list  `git rev-list --tags --no-walk --max-count=1`..HEAD --count)
-    echo "Number of commit from last tag ${commitNumberAfterTag}"
-    currentVersion=$(npx -c 'echo "$npm_package_version"')
-    echo "Current version ${currentVersion}"
-    newVersion=$(echo $currentVersion | sed -e "s/canary\.[[:digit:]]/canary.${commitNumberAfterTag}-${sha}/g")
-    echo "New version ${newVersion}"
-    sed -iE "s/$currentVersion/$newVersion/g" package-lock.json
-    sed -iE "s/$currentVersion/$newVersion/g" package.json
-    sed -iE "s/$currentVersion/$newVersion/g" CHANGELOG.md
-    rm package-lock.jsonE
-    rm package.jsonE
-    rm CHANGELOG.mdE
-    echo "Building..."
-    CI=false npm run build
-    echo "Finish building"
+	echo "Unknown travis mode: ${TRAVIS_MODE}" 1>&2
+	exit 1
 fi
