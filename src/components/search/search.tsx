638b3bb403627fa34b7c11637f076ffa1251e927
Serhii Marchenko <Serhii.marchenko@kaltura.com>
2022-11-16 19:19:49 +0200

    feat(FEV-1572): review search with Dana

--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -1,8 +1,6 @@
 import {h, Component} from 'preact';
-import {A11yWrapper, OnClickEvent} from '@playkit-js/common';
+import {A11yWrapper} from '@playkit-js/common';
 import * as styles from './search.scss';
-import {debounce} from '../../utils';
-const DEBOUNCE_TIMEOUT = 300;
 
 const {withText, Text} = KalturaPlayer.ui.preacti18n;
 const translates = ({activeSearchIndex, totalSearchResults}: SearchProps) => ({
@@ -42,23 +40,17 @@ export interface SearchProps {
 
 interface SearchState {
   active: boolean;
-  focused: boolean;
 }
 
 class SearchComponent extends Component<SearchProps, SearchState> {
   state: SearchState = {
-    active: false,
-    focused: false
+    active: false
   };
   private _inputRef: null | HTMLInputElement = null;
-  private _focusedByMouse = false;
-  private _debouncedOnChange: (value: string) => void;
   constructor(props: SearchProps) {
     super(props);
-    this._debouncedOnChange = debounce(props.onChange, DEBOUNCE_TIMEOUT);
     this.state = {
-      active: false,
-      focused: false
+      active: false
     };
   }
 
@@ -86,26 +78,20 @@ class SearchComponent extends Component<SearchProps, SearchState> {
     this.props.onChange(e.target.value);
   };
 
-  private _onClear = (event: OnClickEvent, byKeyboard?: boolean) => {
-    if (!byKeyboard) {
-      this._focusedByMouse = true;
-    }
+  private _onClear = () => {
     this._inputRef?.focus();
     this.props.onChange('');
   };
 
   private _onFocus = () => {
     this.setState({
-      active: true,
-      focused: !this._focusedByMouse
+      active: true
     });
-    this._focusedByMouse = false;
   };
 
   private _onBlur = () => {
     this.setState({
-      active: false,
-      focused: false
+      active: false
     });
   };
 
@@ -133,31 +119,10 @@ class SearchComponent extends Component<SearchProps, SearchState> {
     onSearchIndexChange(index);
   };
 
-  _handleMouseDown = () => {
-    this._focusedByMouse = true;
-  };
-
   render() {
     const {searchQuery, totalSearchResults, activeSearchIndex} = this.props;
     return (
-      <div
-        className={[styles.searchWrapper, searchQuery || this.state.active ? styles.active : '', this.state.focused ? styles.focused : ''].join(' ')}>
-        <div className={styles.searchIcon}>
-          <svg
-            width="32px"
-            height="32px"
-            viewBox="0 0 32 32"
-            version="1.1"
-            xmlns="http://www.w3.org/2000/svg"
-            xmlnsXlink="http://www.w3.org/1999/xlink">
-            <g id="Icons/32/serch" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-              <path
-                d="M15.1578947,8 C19.1110908,8 22.3157895,11.2046986 22.3157895,15.1578947 C22.3157895,16.8311546 21.7416502,18.3703171 20.7796271,19.5891266 L24.5954583,23.4045417 C24.9243209,23.7334042 24.9243209,24.2665958 24.5954583,24.5954583 C24.2665958,24.9243209 23.7334042,24.9243209 23.4045417,24.5954583 L19.5891266,20.7796271 C18.3703171,21.7416502 16.8311546,22.3157895 15.1578947,22.3157895 C11.2046986,22.3157895 8,19.1110908 8,15.1578947 C8,11.2046986 11.2046986,8 15.1578947,8 Z M15.1578947,9.68421053 C12.1348624,9.68421053 9.68421053,12.1348624 9.68421053,15.1578947 C9.68421053,18.1809271 12.1348624,20.6315789 15.1578947,20.6315789 C18.1809271,20.6315789 20.6315789,18.1809271 20.6315789,15.1578947 C20.6315789,12.1348624 18.1809271,9.68421053 15.1578947,9.68421053 Z"
-                id="Shape"
-                fill="#cccccc"></path>
-            </g>
-          </svg>
-        </div>
+      <div className={[styles.searchWrapper, searchQuery || this.state.active ? styles.active : ''].join(' ')}>
         <input
           className={styles.searchInput}
           aria-label={this.props.searchLabel}
@@ -166,7 +131,6 @@ class SearchComponent extends Component<SearchProps, SearchState> {
           onInput={this._handleOnChange}
           onFocus={this._onFocus}
           onBlur={this._onBlur}
-          onMouseDown={this._handleMouseDown}
           tabIndex={1}
           ref={node => {
             this._inputRef = node;
@@ -175,19 +139,14 @@ class SearchComponent extends Component<SearchProps, SearchState> {
         {searchQuery && (
           <A11yWrapper onClick={this._onClear}>
             <button className={styles.clearIcon} tabIndex={1} aria-label={this.props.clearSearchLabel}>
-              <svg
-                width="32px"
-                height="32px"
-                viewBox="0 0 32 32"
-                version="1.1"
-                xmlns="http://www.w3.org/2000/svg"
-                xmlnsXlink="http://www.w3.org/1999/xlink">
-                <g id="Icons/32/Clere" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                  <path
-                    d="M16,8 C20.418278,8 24,11.581722 24,16 C24,20.418278 20.418278,24 16,24 C11.581722,24 8,20.418278 8,16 C8,11.581722 11.581722,8 16,8 Z M19.8665357,12.1334643 C19.6885833,11.9555119 19.4000655,11.9555119 19.2221131,12.1334643 L16,15.356 L12.7778869,12.1334643 L12.7064039,12.0750737 C12.5295326,11.9582924 12.2891726,11.977756 12.1334643,12.1334643 L12.0750737,12.2049473 C11.9582924,12.3818186 11.977756,12.6221786 12.1334643,12.7778869 L15.356,16 L12.1334643,19.2221131 C11.9555119,19.4000655 11.9555119,19.6885833 12.1334643,19.8665357 C12.3114167,20.0444881 12.5999345,20.0444881 12.7778869,19.8665357 L16,16.644 L19.2221131,19.8665357 L19.2935961,19.9249263 C19.4704674,20.0417076 19.7108274,20.022244 19.8665357,19.8665357 L19.9249263,19.7950527 C20.0417076,19.6181814 20.022244,19.3778214 19.8665357,19.2221131 L16.644,16 L19.8665357,12.7778869 C20.0444881,12.5999345 20.0444881,12.3114167 19.8665357,12.1334643 Z"
-                    id="Shape"
-                    fill="#cccccc"></path>
-                </g>
+              <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M6 12C9.31371 12 12 9.31371 12 6C12 2.68629 9.31371 0 6 0C2.68629 0 0 2.68629 0 6C0 9.31371 2.68629 12 6 12ZM3.38951 2.3502L3.46013 2.41264L5.99548 4.9504L8.53067 2.41264L8.60129 2.3502C8.89537 2.12125 9.32081 2.1418 9.59133 2.41199C9.8844 2.70471 9.88469 3.17958 9.59197 3.47265L7.05523 6.01165L9.59197 8.55189C9.88469 8.84496 9.8844 9.31983 9.59133 9.61255C9.32081 9.88275 8.89537 9.90329 8.60129 9.67434L8.53067 9.61191L5.99548 7.0729L3.46013 9.61191L3.38951 9.67434C3.09543 9.90329 2.66999 9.88275 2.39947 9.61255C2.1064 9.31983 2.10611 8.84496 2.39883 8.55189L4.93498 6.01165L2.39883 3.47265C2.10611 3.17958 2.1064 2.70471 2.39947 2.41199C2.66999 2.1418 3.09543 2.12125 3.38951 2.3502Z"
+                  fill="white"
+                  fill-opacity="0.7"
+                />
               </svg>
             </button>
           </A11yWrapper>
@@ -204,20 +163,12 @@ class SearchComponent extends Component<SearchProps, SearchState> {
                 tabIndex={1}
                 className={`${styles.prevNextButton} ${totalSearchResults === 0 ? styles.disabled : ''}`}
                 aria-label={this.props.prevMatchLabel}>
-                <svg
-                  width="14px"
-                  height="12px"
-                  viewBox="1 0 14 12"
-                  version="1.1"
-                  xmlns="http://www.w3.org/2000/svg"
-                  xmlnsXlink="http://www.w3.org/1999/xlink">
-                  <g id="Icons/16/Arrow/-up" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                    <path
-                      d="M4.78325732,5.37830235 C4.43990319,4.94572127 3.81088342,4.87338855 3.37830235,5.21674268 C2.94572127,5.56009681 2.87338855,6.18911658 3.21674268,6.62169765 L7.21674268,11.6611718 C7.61710439,12.165575 8.38289561,12.165575 8.78325732,11.6611718 L12.7832573,6.62169765 C13.1266115,6.18911658 13.0542787,5.56009681 12.6216977,5.21674268 C12.1891166,4.87338855 11.5600968,4.94572127 11.2167427,5.37830235 L8,9.43097528 L4.78325732,5.37830235 Z"
-                      id="Path-2"
-                      fill="#cccccc"
-                      transform="translate(8.000000, 8.519717) scale(1, -1) translate(-8.000000, -8.519717) "></path>
-                  </g>
+                <svg width="8" height="5" viewBox="0 0 8 5" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    d="M0.292893 4.70711C0.653377 5.06759 1.22061 5.09532 1.6129 4.7903L1.70711 4.70711L4 2.415L6.29289 4.70711C6.65338 5.06759 7.22061 5.09532 7.6129 4.7903L7.70711 4.70711C8.06759 4.34662 8.09532 3.77939 7.7903 3.3871L7.70711 3.29289L4.70711 0.292893C4.34662 -0.0675907 3.77939 -0.0953203 3.3871 0.209705L3.29289 0.292893L0.292893 3.29289C-0.0976311 3.68342 -0.0976311 4.31658 0.292893 4.70711Z"
+                    fill="white"
+                    fill-opacity="0.7"
+                  />
                 </svg>
               </button>
             </A11yWrapper>
@@ -228,19 +179,14 @@ class SearchComponent extends Component<SearchProps, SearchState> {
                 tabIndex={1}
                 className={`${styles.prevNextButton} ${totalSearchResults === 0 ? styles.disabled : ''}`}
                 aria-label={this.props.nextMatchLabel}>
-                <svg
-                  width="14px"
-                  height="12px"
-                  viewBox="1 2 14 12"
-                  version="1.1"
-                  xmlns="http://www.w3.org/2000/svg"
-                  xmlnsXlink="http://www.w3.org/1999/xlink">
-                  <g id="Icons/16/Arrow/down" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                    <path
-                      d="M4.78325732,5.37830235 C4.43990319,4.94572127 3.81088342,4.87338855 3.37830235,5.21674268 C2.94572127,5.56009681 2.87338855,6.18911658 3.21674268,6.62169765 L7.21674268,11.6611718 C7.61710439,12.165575 8.38289561,12.165575 8.78325732,11.6611718 L12.7832573,6.62169765 C13.1266115,6.18911658 13.0542787,5.56009681 12.6216977,5.21674268 C12.1891166,4.87338855 11.5600968,4.94572127 11.2167427,5.37830235 L8,9.43097528 L4.78325732,5.37830235 Z"
-                      id="Path-2"
-                      fill="#cccccc"></path>
-                  </g>
+                <svg width="8" height="5" viewBox="0 0 8 5" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    fill-rule="evenodd"
+                    clip-rule="evenodd"
+                    d="M0.292893 0.292893C0.683417 -0.0976311 1.31658 -0.0976311 1.70711 0.292893L4 2.58579L6.29289 0.292893C6.68342 -0.0976311 7.31658 -0.0976311 7.70711 0.292893C8.09763 0.683417 8.09763 1.31658 7.70711 1.70711L4.70711 4.70711C4.31658 5.09763 3.68342 5.09763 3.29289 4.70711L0.292893 1.70711C-0.0976311 1.31658 -0.0976311 0.683417 0.292893 0.292893Z"
+                    fill="white"
+                    fill-opacity="0.7"
+                  />
                 </svg>
               </button>
             </A11yWrapper>
