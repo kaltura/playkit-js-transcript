1a1b1ea01966509b971c11b91c3d687fc35c07d2
lianbenjamin <79077248+lianbenjamin@users.noreply.github.com>
2023-05-10 10:40:45 +0300

    fix(FEC-13127): a11y - when reaching the autoscroll button from end of script SR does not announce it (#141)
    **the issue:**
    when reaching the autoscroll button from the end of the transcript (from the last item), the SR does not announce the button's aria-label. it should read "Resume AutoScroll".
    
    **root cause:**
    when tabbing to the autoscroll button from the last caption element in the transcript body, for some reason the focused element is the svg, rather than the button itself.
    
    **solution:**
    change the structure of the autoscroll element so that the tooltip will wrap the button.
    Also, exported the autoscroll icon itself outside of the autoscroll button component.
    
    Solves [FEC-13127](https://kaltura.atlassian.net/browse/FEC-13127)
    
    
    [FEC-13127]: https://kaltura.atlassian.net/browse/FEC-13127?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- src/components/autoscroll-button/autoscroll-button.tsx
+++ src/components/autoscroll-button/autoscroll-button.tsx
@@ -1,6 +1,7 @@
 import {h} from 'preact';
 import * as styles from './autoscroll-button.scss';
 import {A11yWrapper, OnClick} from '@playkit-js/common/dist/hoc/a11y-wrapper';
+import {AutoScrollIcon} from "./AutoScrollIcon";
 
 const {withText, Text} = KalturaPlayer.ui.preacti18n;
 const {Tooltip} = KalturaPlayer.ui.components;
@@ -8,7 +9,7 @@ const {Tooltip} = KalturaPlayer.ui.components;
 interface AutoscrollButtonProps {
   onClick: OnClick;
   isAutoScrollEnabled: boolean;
-  setAutoscrollButtonRef: (node: HTMLDivElement | null) => void;
+  setAutoscrollButtonRef: (node: HTMLButtonElement | null) => void;
   autoScrollLabel?: string;
 }
 
@@ -19,27 +20,17 @@ const translates = {
 export const AutoscrollButton = withText(translates)(
   ({onClick, isAutoScrollEnabled, setAutoscrollButtonRef, autoScrollLabel}: AutoscrollButtonProps) => {
     return (
-      <A11yWrapper onClick={onClick}>
-        <div
-          className={`${styles.autoscrollButton} ${isAutoScrollEnabled ? '' : styles.autoscrollButtonVisible}`}
-          tabIndex={isAutoScrollEnabled ? -1 : 1}
-          aria-label={autoScrollLabel}
-          ref={setAutoscrollButtonRef}>
-          <Tooltip label={autoScrollLabel} type="left">
-            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path
-                d="M6.29289 15.2929C6.65338 14.9324 7.22061 14.9047 7.6129 15.2097L7.70711 15.2929L12 19.585L16.2929 15.2929C16.6534 14.9324 17.2206 14.9047 17.6129 15.2097L17.7071 15.2929C18.0676 15.6534 18.0953 16.2206 17.7903 16.6129L17.7071 16.7071L12.7071 21.7071C12.3466 22.0676 11.7794 22.0953 11.3871 21.7903L11.2929 21.7071L6.29289 16.7071C5.90237 16.3166 5.90237 15.6834 6.29289 15.2929Z"
-                fill="white"
-              />
-              <path
-                d="M17.7071 8.70711C17.3466 9.06759 16.7794 9.09532 16.3871 8.7903L16.2929 8.70711L12 4.415L7.70711 8.70711C7.34662 9.06759 6.77939 9.09532 6.3871 8.79029L6.29289 8.70711C5.93241 8.34662 5.90468 7.77939 6.2097 7.3871L6.29289 7.29289L11.2929 2.29289C11.6534 1.93241 12.2206 1.90468 12.6129 2.2097L12.7071 2.29289L17.7071 7.29289C18.0976 7.68342 18.0976 8.31658 17.7071 8.70711Z"
-                fill="white"
-              />
-              <rect x="10" y="10" width="4" height="4" rx="2" fill="white" />
-            </svg>
-          </Tooltip>
-        </div>
-      </A11yWrapper>
+      <div className={styles.autoscrollRoot} tabIndex={isAutoScrollEnabled ? -1 : 1} aria-label={autoScrollLabel}>
+        <Tooltip label={autoScrollLabel} type="left">
+          <A11yWrapper onClick={onClick}>
+            <button
+              className={`${styles.autoscrollButton} ${isAutoScrollEnabled ? '' : styles.autoscrollButtonVisible}`}
+              ref={setAutoscrollButtonRef}>
+              <AutoScrollIcon/>
+            </button>
+          </A11yWrapper>
+        </Tooltip>
+      </div>
     );
   }
 );
