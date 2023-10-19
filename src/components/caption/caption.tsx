cf1ba3053c5e02c240467f79f6dcb6df63bef7bc
Serhii Marchenko <serhii.marchenko@kaltura.com>
2023-10-19 17:48:33 +0300

    fix(FEC-13361): sanitize captions

--- src/components/caption/caption.tsx
+++ src/components/caption/caption.tsx
@@ -1,5 +1,5 @@
 import {Component, h} from 'preact';
-import {A11yWrapper, OnClickEvent} from '@playkit-js/common/dist/hoc/a11y-wrapper';
+import {A11yWrapper} from '@playkit-js/common/dist/hoc/a11y-wrapper';
 import {secondsToTime} from '../../utils';
 import {CuePointData} from '../../types';
 import * as styles from './caption.scss';
